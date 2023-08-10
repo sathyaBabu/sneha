a30f8520608a160e293a6e652cd18eeab8a303a3
Sathya P <sathyasoft123@gmail.com>
2023-08-10 09:29:00 +0530

    Created test3.kt by Sneha

---
+++ test3.kt
@@ -0,0 +1,10 @@
+package sneha
+
+
+fun main(args: Array<String>) {
+    println("Hello World! from test3.kt ")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
